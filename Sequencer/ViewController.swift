3dab6f2c30f8978fd2ef52d6c4fa86590946065f
htbryant94 <htbryant@appcelerator.com>
2017-03-05 19:51:08 +0000

    Initial Commit

---
+++ Sequencer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sequencer
+//
+//  Created by Harry Bryant on 05/03/2017.
+//  Copyright © 2017 Harry Bryant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
