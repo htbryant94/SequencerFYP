cab80daa4298299ccebdf21ff0dc8bb3e9370456
htbryant94 <htbryant@appcelerator.com>
2017-03-05 20:56:48 +0000

    Updated with UI Components

--- Sequencer/ViewController.swift
+++ Sequencer/ViewController.swift
@@ -3,8 +3,11 @@ import UIKit
 import AudioKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    var buttons: [UIButton] = [UIButton]()
+    var grid = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+    var defaultSteps: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     
     // Instrument Data Arrays
     var snareSteps: [Int] = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
@@ -19,12 +22,35 @@ class ViewController: UIViewController {
     var hihatPlayer: AKAudioPlayer!
     
     
+    func updateGridState() {
+        
+        for x in 0 ..< buttonArray.count {
+            
+            if(grid[x] == 0) {
+                
+                buttonArray[x].backgroundColor = UIColor.green
+                
+            } else if(grid[x] == 1) {
+                
+                buttonArray[x].backgroundColor = UIColor.black
+                
+            }
+        }
+        
+    }
+    
     // Variables
     var startLoop = false
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //Append buttons from buttonArray to buttons based on the number of buttons in the Outlet Collection
+        for x in 0 ..< buttonArray.count {
+            self.buttons.append(self.buttonArray[x])
+        }
         
         let kickfile = try! AKAudioFile(readFileName: "EDMkick.mp3", baseDir: .resources)
         let kickfile2 = try! AKAudioFile(readFileName: "EDMkick.mp3", baseDir: .resources)
@@ -105,8 +131,104 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Picker View Stuff
+    
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var label: UILabel!
+    
+    var mode = ["Kick", "Snare"]
     
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return mode[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return mode.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        //let previousSelection = mode[row]
+        
+        label.text = mode[row]
+        
+        if(mode[row] == "Snare") {
+            
+            print("Snare Mode Selected")
+            //print("Previous Selection: \(previousSelection)")
+            
+            for x in 0 ..< 16 {
+                grid[x] = snareSteps[x]
+                
+                updateGridState()
+            }
+            
+        } else if(mode[row] == "Kick") {
+            
+            print("Kick Mode Selected")
+            //print("Previous Selection: \(previousSelection)")
+            
+            for x in 0 ..< 16 {
+                grid[x] = kickSteps[x]
+                
+                updateGridState()
+            }
+   
+        }
+    } // End Picker Stuff
+    
+    // Grid Stuff
+    
+    @IBAction func checkButtonArray(_ sender: AnyObject) {
+        
+        updateGridState()
+    }
+    
+    @IBAction func SaveArray(_ sender: AnyObject) {
+        if(label.text == "Snare") {
+            for x in 0 ..< 16 {
+                snareSteps[x] = grid[x]
+                print("snareSteps \(x) is now: \(snareSteps[x])")
+            }
+        } else if(label.text == "Kick") {
+            for x in 0 ..< 16 {
+                kickSteps[x] = grid[x]
+                print("kickSteps \(x) is now: \(kickSteps[x])")
+            }
+        }
+    }
+    
+    @IBAction func changeButtonState(_ sender: AnyObject) {
+        var value = (sender as! UIButton).tag
+        value = value - 1
+        
+        if grid[value] == 0 {
+            
+            buttonArray[value].backgroundColor = UIColor.black
+            grid[value] = 1
+            
+        } else if(grid[value] == 1) {
+            
+            buttonArray[value].backgroundColor = UIColor.green
+            grid[value] = 0
+        }
+        
+    }
+    
+    
+    @IBAction func checkArray(_ sender: AnyObject) {
+        for x in 0 ..< grid.count {
+            print(grid[x])
+        }
+    }
+    
+    // Create Array of Buttons and store in Outlet Collection
+    @IBOutlet var buttonArray: [UIButton]!
     
 }
 
